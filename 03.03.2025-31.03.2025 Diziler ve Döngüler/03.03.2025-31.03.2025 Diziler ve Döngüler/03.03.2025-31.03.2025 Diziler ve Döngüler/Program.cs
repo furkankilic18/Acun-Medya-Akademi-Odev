304dcddf4d8653c6ebb1b4796c0069936dddaa8a
Furkan KILIÇ <162465390+furkankilic18@users.noreply.github.com>
2025-04-07 00:12:29 +0300

    Diziler ve Döngüler ödevi eklendi

---
+++ 03.03.2025-31.03.2025 Diziler ve Döngüler/03.03.2025-31.03.2025 Diziler ve Döngüler/03.03.2025-31.03.2025 Diziler ve Döngüler/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.WriteLine("1. Sayıyıyı giriniz");
+int sayi1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("2. Sayıyıyı giriniz");
+int sayi2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("3. Sayıyıyı giriniz"); 
+int sayi3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("4. Sayıyıyı giriniz"); 
+int sayi4 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("5. Sayıyıyı giriniz"); 
+int sayi5 = Convert.ToInt32(Console.ReadLine());
+
+int[] sayilar = {sayi1,sayi2,sayi3,sayi4,sayi5};
+double toplam = 0;
+
+foreach (int sayi in sayilar)
+{
+    toplam = toplam +sayi;
+}
+
+
+Console.WriteLine("Girilen 5 sayının ortalaması:"+toplam/5);
