afa6a609d1f5b85e0f6923a019d6147156e81b08
Furkan KILIÇ <162465390+furkankilic18@users.noreply.github.com>
2025-04-07 01:22:12 +0300

    Odev5 yüklendi

---
+++ Odev5/Odev5/Odev1/Odev1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev5.Odev1
+{
+    internal class Odev1
+    {
+        public void yasKontrol(int a)
+        {
+            if (a < 18)
+            {
+                Console.WriteLine("Küçüksünüz");
+            }
+            
+            else if (a >= 18 && a < 35)
+            {
+                Console.WriteLine("Gençsiniz");
+            }
+            
+            else if (a >= 35 && a < 55)
+            {
+                Console.WriteLine("Yetişkinsiniz");
+            }
+            
+            else if (a >= 55 && a < 75)
+            {
+                Console.WriteLine("Yaşlısınız");
+            }
+            
+            else if (a >= 75 && a < 99)
+            {
+                Console.WriteLine("Çok Yaşlısınız");
+            }
+
+            else 
+            {
+                Console.WriteLine("Ya hiç doğmadınız ya da çoktan öldünüz...");
+            }
+        }
+
+    }
+}
