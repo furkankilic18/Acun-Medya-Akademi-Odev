a877cf4ed160ac1d5bd3b0ded5ddc26fc6d656da
Furkan KILIÇ <162465390+furkankilic18@users.noreply.github.com>
2025-04-06 23:56:07 +0300

    First commit

---
+++ Basit Hesap Makinesi/Basit Hesap Makinesi/HesapMakinasi.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basit_Hesap_Makinesi
+{
+    internal class HesapMakinasi
+    {
+        public double topla(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double cikar(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double bolme(double a, double b)
+        {
+            return a / b;
+        }
+
+        public double carpma(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}
