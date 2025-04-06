a418bf7a0152a92bfa2b098a64cc1ab652730bf5
Furkan KILIÇ <162465390+furkankilic18@users.noreply.github.com>
2025-04-07 00:53:36 +0300

    Erişim Belirleyiciler ve Değişkenler ödevi yüklendi

---
+++ Erişim Belirleyiciler ve Değişkenler ile Çalışma/Erişim Belirleyiciler ve Değişkenler ile Çalışma/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Erişim_Belirleyiciler_ve_Değişkenler_ile_Çalışma;
+
+Console.WriteLine("Hello, World!");
+
+// User nesnesi oluştur
+User kullanici = new User();
+
+// Setter metodları ile değerleri ayarla
+kullanici.SetAdSoyad("Furkan KILIÇ");
+kullanici.SetYas(22);
+kullanici.Email = "furkankilic911@gmail.com.com";
+
+// Bilgileri göster
+kullanici.BilgileriGoster();
---
+++ Erişim Belirleyiciler ve Değişkenler ile Çalışma/Erişim Belirleyiciler ve Değişkenler ile Çalışma/User.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Erişim_Belirleyiciler_ve_Değişkenler_ile_Çalışma
+{
+    internal class User
+    {
+        // private değişkenler (sadece bu sınıf içinde erişilebilir)
+        private string adSoyad;
+        private int yas;
+
+        // public değişken (her yerden erişilebilir)
+        public string Email { get; set; }
+
+        // Getter ve Setter metodları
+        public string GetAdSoyad()
+        {
+            return adSoyad;
+        }
+
+        public void SetAdSoyad(string ad)
+        {
+            adSoyad = ad;
+        }
+
+        public int GetYas()
+        {
+            return yas;
+        }
+
+        public void SetYas(int y)
+        {
+            if (y > 0)  // Yaşın negatif olmamasını sağlıyoruz
+                yas = y;
+            else
+                Console.WriteLine("Geçersiz yaş!");
+        }
+
+        // Bilgileri gösteren metot
+        public void BilgileriGoster()
+        {
+            Console.WriteLine($"Ad Soyad: {adSoyad}, Yaş: {yas}, Email: {Email}");
+        }
+    }
+}
+
