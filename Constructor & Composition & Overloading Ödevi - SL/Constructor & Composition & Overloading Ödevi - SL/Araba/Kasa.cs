55801a7a063878d0d25e8a95fb21f45c6d7f9646
Furkan KILIÇ <162465390+furkankilic18@users.noreply.github.com>
2025-04-07 01:19:09 +0300

    Constructor & Composition & Overloading ödevi yüklendi

---
+++ Constructor & Composition & Overloading Ödevi - SL/Constructor & Composition & Overloading Ödevi - SL/Araba/Kasa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor___Composition___Overloading_Ödevi___SL
+{
+    internal class Kasa
+    {
+        String kasa;
+        public Kasa(string kasa)
+        {
+            this.kasa = kasa;
+        }
+
+
+        public override String ToString()
+        {
+            return kasa.ToString();
+        }
+    }
+}
---
+++ Constructor & Composition & Overloading Ödevi - SL/Constructor & Composition & Overloading Ödevi - SL/Araba/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using Constructor___Composition___Overloading_Ödevi___SL;
+
+Marka marka = new Marka("BMW");
+Kasa kasa = new Kasa("Sedan");
+Kapı kapı = new Kapı(4);
+Pencere pencere = new Pencere(4);
+Araba araba = new Araba(marka,"X5",pencere,kapı,kasa,2000000.0);
+
+araba.arabaOzellikleri();
+
